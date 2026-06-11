16ca673c0b027af5814c942caa8bb7b760d6670e
Aditya Kumar <ied10007.23@bitmesra.ac.in>
2026-06-11 23:40:38 +0530

    evaluated baseline performance of random forest and xgboost and saved the models in models with the preprocessor

--- credit_risk/evaluation.py
+++ credit_risk/evaluation.py
@@ -1,8 +1,7 @@
 import numpy as np
+from typing import Any
 from sklearn.metrics import (
     roc_auc_score,
-    accuracy_score,
-    f1_score,
     confusion_matrix,
     precision_score,
     recall_score,
@@ -11,7 +10,7 @@ from sklearn.metrics import (
 )
 
 
-def compute_metrics(y_true: np.array, y_proba: np.array, threshold: float) -> dict[str, any]:
+def compute_metrics(y_true: np.array, y_proba: np.array, threshold: float) -> dict[str, Any]:
     """Returns a dict of all the calculated metrics"""
     y_pred = (y_proba >= threshold).astype(int)
     
@@ -22,12 +21,10 @@ def compute_metrics(y_true: np.array, y_proba: np.array, threshold: float) -> di
         'precision': precision_score(y_true=y_true, y_pred=y_pred),
         'recall': recall_score(y_true=y_true, y_pred=y_pred),
         'confusion_matrix': confusion_matrix(y_true=y_true, y_pred=y_pred),
-        'accuracy': accuracy_score(y_true=y_true, y_pred=y_pred),
-        'f1score': f1_score(y_true=y_true, y_pred=y_pred),   
     }
     
 
-def tune_threshold(y_true: np.array, y_proba: np.array, fn_cost: float, fp_cost: float) -> float:
+def tune_threshold(y_true: np.array, y_proba: np.array, *, fn_cost: float, fp_cost: float) -> float:
     """tunes which threshold is best that minimizes the cost"""
     
     threshols = np.linspace(0.01, 0.99, 99)
@@ -41,3 +38,11 @@ def tune_threshold(y_true: np.array, y_proba: np.array, fn_cost: float, fp_cost:
         
     return threshols[np.argmin(cost)]
 
+def evaluate_model(y_train, train_proba, y_val, val_proba, y_test, test_proba, *, fn_cost, fp_cost):
+    t = tune_threshold(y_val, val_proba, fn_cost=fn_cost, fp_cost=fp_cost)
+    return {
+        "threshold": t,
+        'train': compute_metrics(y_train, train_proba, t),
+        "val": compute_metrics(y_val, val_proba, t),
+        "test": compute_metrics(y_test, test_proba, t),
+    }
